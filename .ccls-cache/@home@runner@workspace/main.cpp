e3bd3d7558f360c3849dafdcaf06f25824a3af0d
sadafrahimi266-cell <sadafrahimi266@gmail.com>
2026-01-13 00:36:21 +0000

    Lab 1: Introduction program

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-   std::cout << "Hello, World!" << std::endl;  
-   return  0;
+    int age;
+
+    cout << "My name is Sadaf Rahimi." << endl;
+    cout << "My goals in life are to graduate and have a successful career." << endl;
+    cout << "My current interests are learning computer science and improving my skills." << endl;
+    cout << "My dreams are to have a stable future and help my family." << endl;
+
+    cout << "23: ";
+    cin >> age;
+
+    cout << "In 1 year, I expect to be more confident in programming." << endl;
+    cout << "In 3 years, I expect to be close to finishing my degree." << endl;
+    cout << "In 5 years, I expect to have a job in my field." << endl;
+    cout << "In 7 years, I expect to be financially independent." << endl;
+
+    cout << "My current age is: " << age << endl;
+
+    return 0;
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# Lab 1 – C++ Introduction Program
+
+## Summary
+This project is a beginner C++ program created for Lab 1. The program prints information about me and takes my age as input using cin.
+YouTube Link:
+
+https://youtu.be/TmJ4ck5V2QsLinks to an external site.
