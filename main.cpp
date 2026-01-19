32f5db0cb20d8279029b4da84a69c546c17fadda
sadafrahimi266-cell <sadafrahimi266@gmail.com>
2026-01-19 00:03:18 +0000

    Final submission for Lab 2

--- main.cpp
+++ main.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 
 int main() {
-    int age;
 
-    cout << "My name is Sadaf Rahimi." << endl;
-    cout << "My goals in life are to graduate and have a successful career." << endl;
-    cout << "My current interests are learning computer science and improving my skills." << endl;
-    cout << "My dreams are to have a stable future and help my family." << endl;
+cout << "welcome to the Amusement Park Ride Eligibility Checker!" << endl; 
+  
+int age;
+  int height;
 
-    cout << "23: ";
-    cin >> age;
-
-    cout << "In 1 year, I expect to be more confident in programming." << endl;
-    cout << "In 3 years, I expect to be close to finishing my degree." << endl;
-    cout << "In 5 years, I expect to have a job in my field." << endl;
-    cout << "In 7 years, I expect to be financially independent." << endl;
-
-    cout << "My current age is: " << age << endl;
-
-    return 0;
+  cout <<"Please enter your age: ";
+  cin >> age;
+  
+  cout <<"please enter your height (in cm) : ";
+  cin >> height;
+ if (age >= 13 && height >= 150) {
+    cout << "you are eligible to ride! Have fun!" << endl;
+  } else {
+    cout << "sorry, you are not eligible to ride." << endl;
+  }
+  return 0;
 }
