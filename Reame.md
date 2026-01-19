8672a29d595245927cf88153fb5b9a0391a847ef
sadafrahimi266-cell <sadafrahimi266@gmail.com>
2026-01-19 00:28:50 +0000

    Final submission lab 2

---
+++ Reame.md
@@ -0,0 +1,10 @@
+# Amusement park ride eligiblity checker
+
+## Summary
+This C++ program asks the user for their age and height and decides if they are eligible for a ride based on the following rules:
+- Age must be 13 or older
+- Height must be 150 or taller 
+it uses cin to get input from the user and if statements to check eligiblity. The program then prints a message tellling the user if they can ride or not.
+## video Demonstration
+[ watch my video](Video link
+https://youtu.be/s5R08JgBwm8)
--- main.cpp
+++ main.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main() {
 
-cout << "welcome to the Amusement Park Ride Eligibility Checker!" << endl; 
-  
-int age;
+  cout << "welcome to the Amusement Park Ride Eligibility Checker!" << endl;
+
+  int age;
   int height;
 
-  cout <<"Please enter your age: ";
+  cout << "Please enter your age: ";
   cin >> age;
-  
-  cout <<"please enter your height (in cm) : ";
+
+  cout << "please enter your height (in cm) : ";
   cin >> height;
- if (age >= 13 && height >= 150) {
+  if (age >= 13 && height >= 150) {
     cout << "you are eligible to ride! Have fun!" << endl;
   } else {
     cout << "sorry, you are not eligible to ride." << endl;
